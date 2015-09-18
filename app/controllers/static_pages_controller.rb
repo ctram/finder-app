021f500859d484de9de2efb3a760ac70952fdfee
chris <chris.tram@gmail.com>
2015-09-18 16:43:54 -0700

    add yelp-api gem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,24 @@ class StaticPagesController < ApplicationController
   def front
   end
 
+  def search
+    client = Yelp::Client.new({
+      consumer_key: 'W1BjNrPnZRlvG6kJaa3dQg',
+      consumer_secret: 'OI7kyEW_uuVw5G3nZZodfY6BjqA',
+      token: '-eERvtTjNeTI4CbOblkP34eAii7JLLpr',
+      token_secret: '9EdVIozIGN_vVRoPTak84QPKxhQ'
+    })
+
+    response = client.search('San Francisco')
+    @response = response.to_json
+    debugger
+
+
+    # // Consumer Key	W1BjNrPnZRlvG6kJaa3dQg
+    # // Consumer Secret	OI7kyEW_uuVw5G3nZZodfY6BjqA
+    # // Token	-eERvtTjNeTI4CbOblkP34eAii7JLLpr
+    # // Token Secret	9EdVIozIGN_vVRoPTak84QPKxhQ
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/', to: 'static_pages#front' 
+  get '/', to: 'static_pages#front'
+  get '/search', to: 'static_pages#search'
 end
