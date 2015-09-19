ebcb2e1328d831d0f28c6f86e65b934e6a169b07
chris <chris.tram@gmail.com>
2015-09-18 21:19:18 -0700

    commit before branching

--- app/assets/javascripts/tubular-app.js
+++ app/assets/javascripts/tubular-app.js
@@ -93,7 +93,7 @@ function getSearchResults (query) {
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
