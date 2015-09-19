637521339bcefd1b4aafd442a1a4c7002e601a74
chris <chris.tram@gmail.com>
2015-09-19 07:31:17 -0700

    fixed why $http was not updating $scope

--- app/assets/javascripts/tubular-app.js
+++ app/assets/javascripts/tubular-app.js
@@ -1,61 +1,32 @@
 var resultsList
 var finderApp = angular.module('finderApp', []);
 
-finderApp.controller('ResultsListController', function () {
-  // Initialization of app
-  var resultsList = this;
-  resultsList.results = [];
-  var defaultQuery = 'San Francisco'
+finderApp.controller('ResultsListController', ['$scope', '$http', function ($scope, $http) {
+  var sampleResult = {name: 'asd', location: {coordinate: {latitude: 'asd', longitude: 'asd'}}};
+  $scope.results = [sampleResult];
 
-  resultsList.submitSearch = function () {
+  $scope.submitSearch = function () {
     var query = $('#input-box').val();
     if (query !== '') {
-      getSearchResults(query);
+      $scope.getSearchResults(query);
     }
-  }
+  };
 
-  function getSearchResults (query) {
-    $.ajax({
-      url: '/search',
-      type: 'get',
-      data: {query: query},
-      success: function (data) {
-        var businesses = data.businesses;
-        var business;
-
-        // resultsList.results = [];
-        for (var i = 0; i < businesses.length; i++) {
-          business = businesses[i];
-          resultsList.results.push(business);
-        }
-        debugger
-      },
-      error: function () {
-        $('#list').html('Something went wrong with the search : [ <BR><BR>Please try again!')
-      }
+  $scope.getSearchResults = function (query) {
+    // Default query
+    if (query === undefined) {
+      query = 'San+Francisco';
+    }
+    var responsePromise = $http.get('/search?query=' + query);
+    responsePromise.success(function (response) {
+      $scope.results = response.businesses;
     });
-  }
+    responsePromise.error(function (response) {
 
-});
+    });
+  };
 
-// function renderResults (data) {
-//   if (data === null) {
-//     return;
-//   }
-//   var businesses = data.businesses;
-//   var business;
-//   var lat;
-//   var long
-//   $list = $('#list');
-//   $list.html('');
-//   for (var i = 0; i < businesses.length; i++) {
-//     business = businesses[i];
-//     name = business.name;
-//     lat = business.location.coordinate.latitude;
-//     long = business.location.coordinate.longitude;
-//     $list.append("<li><b>" + business.name + "</b> Lat: " + lat + "; Long: " + long + "</li>");
-//   }
-// }
+}]);
 
 var map;
 
@@ -67,6 +38,5 @@ function initMap() {
 }
 
 $(document).ready(function () {
-  // getSearchResults();
   initMap();
 });
